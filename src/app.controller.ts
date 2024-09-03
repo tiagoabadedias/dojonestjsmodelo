f5deef8abb37922615183d1868133b0ab614af62
Tiago <tiagoabadedias@hotmail.com>
2024-09-03 14:17:19 -0300

    project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,29 @@
 import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { CreateCustomerDto } from "./customer/customer.dto";
 import { AppService } from "./app.service";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller()
+@ApiTags("customers")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("customer")
+  @Get("customers")
   async customerGet() {
     return await this.appService.getCustomer();
   }
 
-  @Get("customer/:id")
+  @Get("customers/:id")
   async customerGetById(@Param("id") id: string) {
     return await this.appService.getCustomeById(id);
   }
 
-  @Post("customer")
+  @Post("customers")
   async customerCreate(@Body() model: CreateCustomerDto) {
     return await this.appService.createCustomer(model);
   }
 
-  @Delete("customer/:id")
+  @Delete("customers/:id")
   async customerDeleteById(@Param("id") id: string) {
     return await this.appService.deleteCustomeById(id);
   }
